dd91adfd3e76fd4c83b40290c795001fde86c793
Valerya Rivera Díaz <valerydiaz0725@gmail.com>
2026-04-05 21:07:33 -0400

    Update Task.cpp

--- src/Task.cpp
+++ src/Task.cpp
@@ -3,9 +3,12 @@
 
 using namespace std;
 
+// contructor por defecto 
+// se iknician valores basicos 
+
 Task::Task() : id(0), priority(3), status("Pending") {
 }
-
+// constructor con parametros 
 Task::Task(int id, string title, string description, string course, int priority, string dueDate, string status) {
     this->id = id;
     this->title = title;
@@ -15,35 +18,35 @@ Task::Task(int id, string title, string description, string course, int priority
     this->dueDate = dueDate;
     this->status = status;
 }
-
+// devuelve id
 int Task::getId() const {
     return id;
 }
-
+// devuelve nombre de la tarea 
 string Task::getTitle() const {
     return title;
 }
-
+// descripcion 
 string Task::getDescription() const {
     return description;
 }
-
+// que curso pertenece la tarea 
 string Task::getCourse() const {
     return course;
 }
-
+// devuleve prioridad 
 int Task::getPriority() const {
     return priority;
 }
-
+// devuelve fecha de entrega 
 string Task::getDueDate() const {
     return dueDate;
 }
-
+// devuelve estatus 
 string Task::getStatus() const {
     return status;
 }
-
+// muestra en pantalla toda la info de la tarea 
 void Task::display() const {
     cout << "[" << id << "] " << title << "\n";
     cout << "Course: " << course << "\n";
