b16d087bc6c6ff7b81535747816ca7094e22db3b
Valerya Rivera Díaz <valerydiaz0725@gmail.com>
2026-04-05 21:04:02 -0400

    Update TaskList.cpp

--- src/TaskList.cpp
+++ src/TaskList.cpp
@@ -2,18 +2,18 @@
 #include "TaskList.h"
 
 using namespace std;
-
+// estructura nodo
 struct Node {
     Task data;
     Node* next;
 };
-
+// constructor de la clae
 TaskList::TaskList() : head(nullptr) {
 }
-
+// destructor 
 TaskList::~TaskList() {
     Node* current = head;
-
+// recorre todita la lista y elimina nodos
     while (current != nullptr) {
         Node* temp = current;
         current = current->next;
@@ -21,7 +21,7 @@ TaskList::~TaskList() {
         temp = nullptr;
     }
 }
-
+// funcin para ver si un id ya existe 
 bool TaskList::idExists(int id) const {
     Node* current = head;
 
@@ -34,15 +34,15 @@ bool TaskList::idExists(int id) const {
 
     return false;
 }
-
+// funcion para crear nueva tarea 
 void TaskList::createTask(const Task& task) {
     if (idExists(task.getId())) {
         cout << "ID already exists.\n";
         return;
     }
-
+// se crea u un nuevo nodo
     Node* newNode = new Node{task, nullptr};
-
+// lista vacia 
     if (head == nullptr) {
         head = newNode;
     }
@@ -52,13 +52,13 @@ void TaskList::createTask(const Task& task) {
         while (current->next != nullptr) {
             current = current->next;
         }
-
+// ultimo nodo apunta al nuevo
         current->next = newNode;
     }
 
     cout << "Task created succesfully!\n";
 }
-
+// veamos todas las tareas registradas 
 void TaskList::showTasks() const {
     cout << "--- SHOW ALL TASKS ---\n";
 
@@ -68,14 +68,14 @@ void TaskList::showTasks() const {
     }
 
     Node* current = head;
-
+// recorre lista 
     while (current != nullptr) {
         current->data.display();
         cout << "--------------------------\n";
         current = current->next;
     }
 }
-
+// funcion para buscar tarea por el id 
 Task* TaskList::searchById(int id) {
     Node* current = head;
 
@@ -88,12 +88,12 @@ Task* TaskList::searchById(int id) {
 
     return nullptr;
 }
-
+// funcion para eliminar una tarea, true si si se elimino, false si no 
 bool TaskList::deleteTask(int id) {
     if (head == nullptr) {
         return false;
     }
-
+// si la tarea se elimio esta en el primer nodo 
     if (head->data.getId() == id) {
         Node* temp = head;
         head = head->next;
@@ -102,15 +102,15 @@ bool TaskList::deleteTask(int id) {
     }
 
     Node* current = head;
-
+// recorrer lista hasta encontrar el nodo que se quiere eliminar 
     while (current->next != nullptr && current->next->data.getId() != id) {
         current = current->next;
     }
-
+// no hay id? FALSE
     if (current->next == nullptr) {
         return false;
     }
-
+// se elimina el nodo 
     Node* temp = current->next;
     current->next = current->next->next;
     delete temp;
