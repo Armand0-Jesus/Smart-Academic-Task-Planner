a6d23ae8f54b6ec9627059feaacc337ca5feb388
Armando Rodríguez <armandorodz23@gmail.com>
2026-04-06 10:19:53 -0400

    Refactor comments
    Updated comments for clarity and corrected typos.

--- src/Task.cpp
+++ src/Task.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 // contructor por defecto 
-// se iknician valores basicos 
+// se inician valores basicos 
 
 Task::Task() : id(0), priority(3), status("Pending") {
 }
@@ -18,31 +18,31 @@ Task::Task(int id, string title, string description, string course, int priority
     this->dueDate = dueDate;
     this->status = status;
 }
-// devuelve id
+// getters para clase Task
 int Task::getId() const {
     return id;
 }
-// devuelve nombre de la tarea 
+
 string Task::getTitle() const {
     return title;
 }
-// descripcion 
+ 
 string Task::getDescription() const {
     return description;
 }
-// que curso pertenece la tarea 
+
 string Task::getCourse() const {
     return course;
 }
-// devuleve prioridad 
+
 int Task::getPriority() const {
     return priority;
 }
-// devuelve fecha de entrega 
+
 string Task::getDueDate() const {
     return dueDate;
 }
-// devuelve estatus 
+
 string Task::getStatus() const {
     return status;
 }
