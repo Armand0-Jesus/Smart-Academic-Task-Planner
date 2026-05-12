a039062a415c65a032e3e22af72a0b0f0fe0d7d9
Armando Rodríguez <armandorodz23@gmail.com>
2026-05-12 18:42:07 -0400

    refactor TaskList class
    Added new methods for task management functionality.

--- include/Colegial Task Manager/TaskList.h
+++ include/Colegial Task Manager/TaskList.h
@@ -8,16 +8,25 @@ struct Node;
 class TaskList {
 private:
     Node* head;
-    bool idExists(int id) const;
-
+    
 public:
     TaskList();
     ~TaskList();
 
+    bool idExists(int id) const; // anadida
+  
     void createTask(const Task& task);
+    void addTask(const Task& task); // anadida
     void showTasks() const;
+    void displayAll() const; // anadida
     Task* searchById(int id);
     bool deleteTask(int id);
+    bool markCompleted(int id); // anadida
+    bool Empty() const; // anadida
 };
 
 #endif
+
+
+
+
