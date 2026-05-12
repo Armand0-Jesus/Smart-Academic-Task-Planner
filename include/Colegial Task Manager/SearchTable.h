121a4ba471acc9dd9a1c63b1ce125331576d7c97
Armando Rodríguez <armandorodz23@gmail.com>
2026-05-12 18:46:42 -0400

    add SearchTable header file for task management

---
+++ include/Colegial Task Manager/SearchTable.h
@@ -0,0 +1,38 @@
+#ifndef SEARCHTABLE_H
+#define SEARCHTABLE_H
+
+#include <iostream>
+#include "Task.h"
+using namespace std;
+
+const int TABLE_SIZE = 101;
+
+struct HashNode {
+    int key;
+    Task* taskPtr;
+    HashNode* next;
+
+    HashNode(int key, Task* taskPtr) {
+        this->key = key;
+        this->taskPtr = taskPtr;
+        next = nullptr;
+    }
+};
+
+class SearchTable {
+private:
+    HashNode* table[TABLE_SIZE];
+
+    int hashFunction(int key) const;
+
+public:
+    SearchTable();
+    ~SearchTable();
+
+    void insert(int key, Task* taskPtr);
+    Task* search(int key) const;
+    bool remove(int key);
+    void displayTable() const;
+};
+
+#endif
