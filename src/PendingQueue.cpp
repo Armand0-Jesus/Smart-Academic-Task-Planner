848202180d360ac97516c79ac9a2cd75d13cbc35
Armando Rodríguez <armandorodz23@gmail.com>
2026-05-12 18:49:47 -0400

    add PendingQueue class with enqueue and dequeue

---
+++ src/PendingQueue.cpp
@@ -0,0 +1,74 @@
+#include "PendingQueue.h"
+
+PendingQueue::PendingQueue() {
+    front = nullptr;
+    rear = nullptr;
+}
+
+PendingQueue::~PendingQueue() {
+    while (front != nullptr) {
+        QueueNode* temp = front;
+        front = front->next;
+        delete temp;
+        temp = nullptr;
+    }
+
+    rear = nullptr;
+}
+
+void PendingQueue::enqueue(Task task) {
+    QueueNode* newNode = new QueueNode(task);
+
+    if (rear == nullptr) {
+        front = newNode;
+        rear = newNode;
+    }
+    else {
+        rear->next = newNode;
+        rear = newNode;
+    }
+}
+
+bool PendingQueue::dequeue() {
+    if (front == nullptr) {
+        return false;
+    }
+
+    QueueNode* temp = front;
+    front = front->next;
+
+    if (front == nullptr) {
+        rear = nullptr;
+    }
+
+    delete temp;
+    temp = nullptr;
+    return true;
+}
+
+void PendingQueue::display() const {
+    if (front == nullptr) {
+        cout << "No hay tareas pendientes." << endl;
+        return;
+    }
+
+    QueueNode* current = front;
+    int number = 1;
+
+    cout << "--- Tareas Pendientes ---" << endl;
+
+    while (current != nullptr) {
+        cout << number << ". [" << current->data.getId() << "] "
+             << current->data.getTitle()
+             << " | Prioridad: " << current->data.getPriorityText()
+             << " | Fecha: " << current->data.getDueDate() << endl;
+
+        current = current->next;
+        number++;
+    }
+}
+
+bool PendingQueue::Empty() const {
+    return front == nullptr;
+}
+
