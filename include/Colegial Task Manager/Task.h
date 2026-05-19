c367f011e7838a65b1d28fd15c3e8e5431faa192
Armando Rodríguez <armandorodz23@gmail.com>
2026-05-19 12:36:40 -0400

    task documentation
    Added detailed documentation for the Task class and its methods.

--- include/Colegial Task Manager/Task.h
+++ include/Colegial Task Manager/Task.h
@@ -1,9 +1,22 @@
+/**
+ * @file Task.h
+ * @brief Define la clase Task usada para representar una tarea academica
+ */
+
 #ifndef TASK_H
 #define TASK_H
 
 #include <string>
 using namespace std;
 
+/**
+ * @class Task
+ * @brief Representa una tarea academica en el planificador academico
+ *
+ * Guarda toda la informacion basica relacionada con las tareas,
+ * incluye ID, titulo, descripcion, curso, prioridad, fecha de vencimiento
+ * y estado actual.
+ */
 class Task {
 private:
     int id;
@@ -15,8 +28,31 @@ private:
     string status;
 
 public:
+    /**
+     * @brief Constructor por defecto.
+     */
     Task();
+
+    /**
+     * @brief Constructor parametrizado.
+     *
+     * Crea un objeto con toda la información requerida para la tarea.
+     *
+     * @param id Identifiacdor unico para la tarea.
+     * @param title Titulo de la tarea.
+     * @param description Descripcion corta de la tarea.
+     * @param course Curso/Clase relacionada con la tarea.
+     * @param priority Nivel de prioridad de la tarea.
+     * @param dueDate Fecha de vencimiento de la tarea.
+     * @param status Estado actual de la tarea.
+     */
     Task(int id, string title, string description, string course, int priority, string dueDate, string status);
+
+    /** 
+     * @name Getters
+     * @brief Funciones para acceder a atributos de tareas.
+     * @{
+     */
 
     int getId() const;
     string getTitle() const;
@@ -27,6 +63,13 @@ public:
     string getStatus() const;
     string getPriorityText() const;
 
+    /** @}  */
+
+    /** 
+     * @name Setters
+     * @brief Funciones para modificar atributos de tareas.
+     * @{
+     */
 
     void setTitle(string title);
     void setDescription(string description);
@@ -35,7 +78,11 @@ public:
     void setDueDate(string dueDate);
     void setStatus(string status);
 
+    /** @}  */
 
+    /**
+     * @brief Muestra en pantalla la información de la tarea.
+     */
     void display() const;
    
 };
