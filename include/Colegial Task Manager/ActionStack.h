265f4ca62b99220ec0b25b4840a9af38a39a3f41
Armando Rodríguez <armandorodz23@gmail.com>
2026-05-19 13:57:43 -0400

    enhance ActionStack.h with documentation comments
    Added documentation comments for the ActionStack class and its methods.

--- include/Colegial Task Manager/ActionStack.h
+++ include/Colegial Task Manager/ActionStack.h
@@ -1,23 +1,60 @@
+/**
+ * @file ActionStack.h
+ * @brief Define la clase ActionStack para el historial de acciones
+ */
 #ifndef ACTIONSTACK_H
 #define ACTIONSTACK_H
 
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 struct ActionNode;
 
+/**
+ * @class ActionStack
+ * @brief Implementa una pila (stack) para el historial de acciones recientes
+ *
+ * La clase es usada para grabar eventos como la creacion,
+ * eliminacion, busqueda o finalizacion de tareas.
+ */
 class ActionStack {
 private:
     ActionNode* top;
 
 public:
+    /**
+     * @brief Crea una pila vacia de accion.
+     */
     ActionStack();
+
+     /**
+     * @brief Destruye la memoria alocada que utilizaba el stack.
+     */
     ~ActionStack();
 
+     /**
+     * @brief Agrega la nueva accion al tope de la pila.
+     * @param action Descripcion de la accion realizada
+     */
     void push(string action);
+
+     /**
+     * @brief Remueve la accion mas reciente de la pila.
+     * @return true si la accion fue removida, false sino.
+     */
     bool pop();
+
+     /**
+     * @brief Muestra el historial de acciones en orden de nuevo a mas viejo.
+     */
     void display() const;
+
+     /**
+     * @brief Revisa si ActionStack esta vacio.
+     * @return true si la pila esta vacio, false sino.
+     */
     bool Empty() const;
 };
 
