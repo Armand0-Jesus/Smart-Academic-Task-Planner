fda171d4f194a90938af7bd898de5dcc7e07cc3e
Valerya Rivera Díaz <valerydiaz0725@gmail.com>
2026-05-26 20:41:25 -0400

    Update TaskList.cpp

--- src/TaskList.cpp
+++ src/TaskList.cpp
@@ -1,6 +1,7 @@
 #include "TaskList.h"
-
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 struct Node {
@@ -151,5 +152,94 @@ bool TaskList::markCompleted(int id) {
 
 bool TaskList::Empty() const {
     return head == nullptr;
+
+void TaskList::saveToFile(const string& filename) const {
+    ofstream file(filename);
+
+    if (!file) {
+        cout << "ERROR: No se pudo abrir el archivo para guardar datos.\n";
+        return;
+    }
+
+    Node* current = head;
+
+    while (current != nullptr) {
+        file << current->data.getId() << "|"
+             << current->data.getTitle() << "|"
+             << current->data.getDescription() << "|"
+             << current->data.getCourse() << "|"
+             << current->data.getPriority() << "|"
+             << current->data.getDueDate() << "|"
+             << current->data.getStatus() << endl;
+
+        current = current->next;
+    }
+
+    file.close();
+    cout << "Los datos fueron guardados correctamente en el archivo.txt\n";
+}
+
+void TaskList::loadFromFile(const string& filename) {
+    ifstream file(filename);
+
+    if (!file) {
+        cout << "AVISO: No se encontro un archivo previo guardado. Se comenzara en una lista vacia.\n";
+        return;
+    }
+
+    string line;
+
+    while (getline(file, line)) {
+        stringstream ss(line);
+
+        string idStr;
+        string title;
+        string description;
+        string course;
+        string priorityStr;
+        string dueDate;
+        string status;
+
+        getline(ss, idStr, '|');
+        getline(ss, title, '|');
+        getline(ss, description, '|');
+        getline(ss, course, '|');
+        getline(ss, priorityStr, '|');
+        getline(ss, dueDate, '|');
+        getline(ss, status, '|');
+
+        if (idStr.empty() || title.empty() || description.empty() ||
+            course.empty() || priorityStr.empty() || dueDate.empty() || status.empty()) {
+            cout << "AVISO: Hay informacion incompleta.\n";
+            continue;
+        }
+
+        int id;
+        int priority;
+
+        try {
+            id = stoi(idStr);
+            priority = stoi(priorityStr);
+        } catch (...) {
+            cout << "AVISO: Hay informacion invalida.\n";
+            continue;
+        }
+
+        if (priority < 1 || priority > 3) {
+            cout << "AVISO: La prioridad de la tarea es invalida.\n";
+            continue;
+        }
+
+        if (!idExists(id)) {
+            Task task(id, title, description, course, priority, dueDate, status);
+            createTask(task);
+        }
+    }
+
+    file.close();
+    cout << "Los datos fueron cargados correctamente desde el archivo.txt\n";
+}
+
+    
 }
 
